a5787dfaf560b8634134004d123d6cb21b7e4fbc
tuananh230197zxc <88013981+tuananh230197zxc@users.noreply.github.com>
2023-03-28 23:17:06 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,7 +1,7 @@
-const nameGirl = 'Em';
+const nameGirl = '';
 const giftUrl = 'https://www.facebook.com/profile.php?id=100011356655214';
-const eventName = 'Chúc mừng sinh nhật';
+const eventName = 'Chúc mừng sinh nhật Yến <3';
 const titleCard = 'Tặng người ấy';
-const contentCard = 'Chúc Yến sinh nhật tràn ngập niềm vui và những nụ cười. Mong điều đẹp nhất sẽ đến với em trong hôm nay và cả những ngày sau';
+const contentCard = 'Chúc Yến sinh nhật tràn ngập niềm vui và những nụ cười. Mong điều đẹp nhất sẽ đến với em trong hôm nay và cả những ngày sau <3';
 const giftImage = 'hot-girl.png';
 
