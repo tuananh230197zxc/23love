06e1e59984e4167ef9a207a371e6be3b67204b06
tuananh230197zxc <88013981+tuananh230197zxc@users.noreply.github.com>
2023-04-07 22:05:05 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 const nameGirl = '';
-const giftUrl = 'https://www.facebook.com/profile.php?id=100014680295356';
+const giftUrl = '';
 const eventName = 'Lời tỏ tình <3';
 const titleCard = 'Tặng lươn chúa';
 const contentCard = 'Cảm ơn định mệnh đã cho anh gặp e,anh không chắc tương lai hai đứa mình ra sao,nhưng anh sẽ làm hết sức mọi điều để khiến em bên anh thật hạnh phúc.Hiền ơi,em nhận lời làm người yêu anh nhé<3 <3';
