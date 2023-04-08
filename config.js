e17b12f0007d07ad18b66b8ec4b7689c93f664d6
tuananh230197zxc <88013981+tuananh230197zxc@users.noreply.github.com>
2023-04-08 07:35:43 +0700

    Update config.js

--- config.js
+++ config.js
@@ -2,6 +2,6 @@ const nameGirl = 'Hiền Lươn Chúa';
 const giftUrl = '';
 const eventName = 'Kỷ niệm đầu tiên';
 const titleCard = 'Lời tỏ tình';
-const contentCard = 'Anh dám hứa chắc tương lai mình sẽ ra sao,nhưng a sẽ cố gắng làm em thật hạnh phúc khi bên anh.Hiền ơi,em nhận lời làm người yêu anh nhé <3';
+const contentCard = 'Anh không dám hứa chắc tương lai mình sẽ ra sao,nhưng a sẽ cố gắng làm em thật hạnh phúc khi bên anh.Hiền ơi,em nhận lời làm người yêu anh nhé <3';
 const giftImage = 'hot-girl.png';
 
